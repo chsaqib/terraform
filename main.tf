a9ff2bdcd13616d6919ebe8567771b037cc63c23
saqib nazir <saqib_9090@yahoo.com>
2022-12-10 16:12:58 +0000

    Using default route table

--- main.tf
+++ main.tf
@@ -30,8 +30,8 @@ resource "aws_subnet" "myapp-subnet-1"{
     }
 }
 
-# creating route table
-resource "aws_route_table" "myapp-route-table" {
+# # creating route table
+/*resource "aws_route_table" "myapp-route-table" {
   vpc_id = aws_vpc.myapp-vpc.id
 
   route {
@@ -41,7 +41,7 @@ resource "aws_route_table" "myapp-route-table" {
   tags={
     Name= "${var.env_prefix}-rtb"
   }
-}
+}*/
 
 # Creating Internet gateway
 resource "aws_internet_gateway" "myapp-igw" {
@@ -52,8 +52,23 @@ resource "aws_internet_gateway" "myapp-igw" {
     
 }
 
-# Creaing Route table association
-resource "aws_route_table_association" "a-rtb-subnet" {
-  subnet_id = aws_subnet.myapp-subnet-1.id
-  route_table_id = aws_route_table.myapp-route-table.id
+# # Creaing Route table association
+# resource "aws_route_table_association" "a-rtb-subnet" {
+#   subnet_id = aws_subnet.myapp-subnet-1.id
+#   route_table_id = aws_route_table.myapp-route-table.id
+# }
+
+# Using default route table
+
+resource "aws_default_route_table" "main-rtb" {
+  default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
+  
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.myapp-igw.id
+  }
+  tags={
+    Name= "${var.env_prefix}-main-rtb"
+  }
+
 }
